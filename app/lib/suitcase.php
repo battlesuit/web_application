1a1d79cb0c20c429b328ad8c76f15ce9db9596cf
Thomas Monzel <tom.monzel@gmail.com>
2012-11-20 23:54:27 +0100

    Removed unused autoload

--- app/lib/suitcase.php
+++ app/lib/suitcase.php
@@ -1,8 +1,6 @@
 <?php
 namespace suitcase {
   require __DIR__."/../../vendors/suitcase.php";
-
   import('http-application');
-  autoload('application\Base', __DIR__."/application/base.php");
 }
 ?>
